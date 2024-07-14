73c6b1bfb1ad8a7a26e9347f50f4d5991cfde8b0
sathish kumar <skch2710@gmail.com>
2024-07-14 13:29:49 +0530

    Update RegexUtil.java

--- src/main/java/com/skch/skchhostelservice/util/RegexUtil.java
+++ src/main/java/com/skch/skchhostelservice/util/RegexUtil.java
@@ -115,6 +115,24 @@ public class RegexUtil {
 				input.replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase() : "";
 	}
 	
+	public static String titleCase(String input) {
+        if (input == null || input.isBlank()) {
+            return "";
+        }
+        String result = input.replaceAll("([a-z])([A-Z])", "$1 $2");
+        result = result.substring(0, 1).toUpperCase() + result.substring(1);
+        return result;
+    }
+	
+	public static String camelCase(String input) {
+        if (input == null || input.isEmpty()) {
+            return "";
+        }
+        String result = input.replaceAll(" ", "");
+        result = result.substring(0, 1).toLowerCase() + result.substring(1);
+        return result;
+    }
+	
 	public static String capitalize(String input) {
 		if (input != null && !input.isEmpty()) {
 			Pattern pattern = Pattern.compile("(\\b\\w)(\\w*)");
